f2ef013e3ae3c85949bdad2e225a0852c3f674f9
Sampath Kumar <9948971770sampath@gmail.com>
2025-05-19 19:41:47 +0530

    Code push to repo

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','ucdavis.edu','ufp.pt','indiana.edu','berkeley.edu','uct.ac.za','mac.com','columbia.edu','gmail.com','unl.edu'],
+['2006',14,10,11,7,5,2,3,1,2,0],
+['2006',14,10,11,7,5,2,3,1,2,0]
+];
